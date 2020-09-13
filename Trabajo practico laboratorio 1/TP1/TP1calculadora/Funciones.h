cdd96cb5b23450cbfdca9cbc32d6541c7186615e
MatiasCossu <cossumatias1@gmail.com>
2020-09-12 22:55:14 -0300

    Se comento las funciones

--- Trabajo practico laboratorio 1/TP1/TP1calculadora/Funciones.h
+++ Trabajo practico laboratorio 1/TP1/TP1calculadora/Funciones.h
@@ -1,22 +1,41 @@
 float SumaDeNumeros(float, float);
-/** \brief Suma los dos datos
+/** \brief Suma los datos
  *
- * \param Float es el primer dato ingresado.
- * \param Float es el segundo dato ingresado.
- * \return devuelve el resultado de la suma de los datos
+ * \param Float es el dato ingresado.
+ * \param Float es el dato ingresado.
+ * \return devuelve el resultado de la suma de los datos.
  */
 
 float RestaDeNumeros(float, float);
 /** \brief Se restan los datos
  *
- * \param Float es el primer dato ingresado.
- * \param Float es el segundo dato ingresado.
- * \return devuelve el resultado de la suma de los datos.
+ * \param Float es el dato ingresado.
+ * \param Float es el dato ingresado.
+ * \return devuelve el resultado de la resta de los datos.
  */
 
 
 float DivisionDeNumeros(float, float);
 
+/** \brief Se dividen los datos
+ *
+ * \param Float es el  dato ingresado.
+ * \param Float es el  dato ingresado.
+ * \return devuelve el resultado de la divison de los datos y si uno de los datos es el numero cero, return devuelve un cero
+ */
+
 float MultiplicacionDeNumeros(float, float);
+/** \brief Se multiplican los datos
+ *
+ * \param Float es el dato ingresado.
+ * \param Float es el dato ingresado.
+ * \return devuelve el resultado de la multiplicacion de los datos.
+ */
 
 float FactorialNumero(float);
+
+/** \brief Se saca el factorial del dato pedido.
+
+ * \param float es el dato ingresado
+ * \return se devuelve el resultado del factorial.
+ */
